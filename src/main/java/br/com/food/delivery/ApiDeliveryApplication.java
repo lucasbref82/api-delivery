0350d10ece44f9a496b06d613ef407b3506a19cb
Lucas Brenner <lucas.ferreira@geosite.com.br>
2023-10-05 16:46:42 -0300

    Primeiro commit

---
+++ src/main/java/br/com/food/delivery/ApiDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.food.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeliveryApplication.class, args);
+	}
+
+}
