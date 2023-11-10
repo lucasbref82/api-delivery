7e2c78e84a504b3ec126edddf750269a2bcb5efa
Lucas Brenner <lucas.ferreira@geosite.com.br>
2023-11-10 10:18:36 -0300

    Melhoriaas na ApiExceptionHandler

--- src/main/java/br/com/food/delivery/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/food/delivery/exceptionhandler/ApiExceptionHandler.java
@@ -1,50 +1,109 @@
 package br.com.food.delivery.exceptionhandler;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import br.com.food.delivery.domain.exception.EntidadeEmUsoException;
 import br.com.food.delivery.domain.exception.EntidadeNaoEncontradaException;
+import br.com.food.delivery.domain.exception.NegocioException;
+import br.com.food.delivery.enumeration.ProblemType;
 
 @ControllerAdvice
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
-	
-	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-	
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request){
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		Throwable throwable = ExceptionUtils.getRootCause(ex);
+		if (throwable instanceof InvalidFormatException) {
+			return handleInvalidFormatException((InvalidFormatException) throwable, headers, status, request);
+		}
+		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		String detail = "O corpo da requisição está válido. Verifique erro de sintaxe";
+		Problema problema = criarBuilderProblema(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problema, headers, status, request);
 	}
-	
+
+	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		String path = ex.getPath().stream()
+				.map(ref -> ref.getFieldName())
+				.collect(Collectors.joining("."));
+		
+		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		String detail = String.format("A propriedade '%s' recebeu o valor '%s', "
+				+ "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
+				path, ex.getValue(), ex.getTargetType().getSimpleName());
+		
+		Problema problema = criarBuilderProblema(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problema, headers, status, request);
+	}
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> handleTratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,
+			WebRequest request) {
+		// Padrão RFC 7807
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+		String detail = ex.getMessage();
+		Problema problema = criarBuilderProblema(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
+		HttpStatus httpStatus = HttpStatus.CONFLICT;
+		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
+		String detail = ex.getMessage();
+		Problema problema = criarBuilderProblema(httpStatus, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), httpStatus, request);
+	}
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request) {
+		HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+		String detail = ex.getMessage();
+		Problema problema = criarBuilderProblema(httpStatus, problemType, detail).build();
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), httpStatus, request);
+	}
+
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<?> tratarViolacaoDeContraints(DataIntegrityViolationException ex, WebRequest request) {
 		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
 	}
-	
+
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
-		
+
 		if (body == null) {
-			body = Problema.builder()
-					.dataHora(formatter.format(LocalDateTime.now()))
-					.mensagem(status.getReasonPhrase())
-					.build();
+			body = Problema.builder().status(status.value()).title(status.getReasonPhrase()).build();
 		} else if (body instanceof String) {
-			body = Problema.builder()
-					.dataHora(formatter.format(LocalDateTime.now()))
-					.mensagem((String) body)
-					.build();
+			body = Problema.builder().status(status.value()).title((String) body).build();
 		}
-		
+
 		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+
+	private Problema.ProblemaBuilder criarBuilderProblema(HttpStatus status, ProblemType problemType, String detail) {
+		return Problema.builder().status(status.value()).type(problemType.getUri()).title(problemType.getTitle())
+				.detail(detail);
 	}
 }
--- src/main/java/br/com/food/delivery/exceptionhandler/Problema.java
+++ src/main/java/br/com/food/delivery/exceptionhandler/Problema.java
@@ -1,13 +1,24 @@
 package br.com.food.delivery.exceptionhandler;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
+// Excluir objetos nulos na representação da resposta
+@JsonInclude(value = Include.NON_NULL)
 @Data
 @Getter
 @Builder
 public class Problema {
-	private String dataHora;
-	private String mensagem;
+	// Status HTTP
+	private Integer status;
+	// Tipo do problema ou URL contendo informações de como resolver o problema
+	private String type;
+	// Titulo do problema
+	private String title;
+	// Detalhe da excessão
+	private String detail;
 }
